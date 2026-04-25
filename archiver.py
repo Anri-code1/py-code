e6abfb069a0e4e1a787a76877d450eaaffaadd10
Anri-code1 <anri11@internet.ru>
2026-04-25 18:34:05 +0300

    закончил часть работы, эта функция создаёт архив из папки

---
+++ archiver.py
@@ -0,0 +1,29 @@
+import os
+import struct
+
+def creat(archive, folder):
+	files = []
+	for f in os.listdir(folder):          #переюираем всё что есть в папке
+		full_path = os.path.join(folder, f)     #берём полный путь
+		if os.path.isfile(full_path):           #это файл?
+			files.append(f)
+	with open(archive, "wb") as arc:                 #бинарный режим wb
+		arc.write(struct.pack("I", len(files)))      #в архиватор записываем количество файлов (int)
+
+
+		for f in files:
+			filepath = os.path.join(folder, f)
+			name_b = f.encode("utf-8")                   #имя в байтах
+			arc.write(struct.pack("I", len(name_b)))     #пишем длину имени целым числом(int)
+			arc.write(name_b)
+
+			filesize = os.path.getsize(filepath)
+			arc.write(struct.pack("Q", filesize))        #пишем размер файла большим числом(8 байт)
+
+			with open(filepath, "rb") as f:
+				arc.write(f.read())
+	print(f"Архив {archive} создан. Файлов: {len(files)}")
+
+
+
+creat("test.arc", "/data/data/com.termux/files/home/test_folder")
